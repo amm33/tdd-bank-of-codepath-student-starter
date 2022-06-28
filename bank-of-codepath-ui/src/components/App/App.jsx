0a8b82fe9e4630d6ccf960bde61672b81911a472
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-06-28 15:50:51 -0700

    finally got feature 1

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,35 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TransactionDetail from "../TransactionDetail/TransactionDetail";
+import { useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(null);
+  const [transactions, setTransactions] = useState([]);
+  const [transfer, setTransfer] = useState([]);
+  const [error, setError] = useState("");
+  const [filterInputValue, setFilterInputValue] = useState("");
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
+    <div className="app">
+      <BrowserRouter>
+        <Navbar
+          filterInputValue={filterInputValue}
+          setFilterInputValue={setFilterInputValue}
+        />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route
+              path="/transactions:transactionId"
+              element={<TransactionDetail />}
+            ></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
